e15b3a4d06a95828c5acbbf98b3cbe40c68dd498
Yunus Meriç <ahmiyuya0409@gmail.com>
2025-08-06 01:38:44 +0300

    Project is updated.

--- Hackathon/hackv2/hack/src/main/java/com/hackathon/hack/service/LangChainService.java
+++
@@ -1,7 +0,0 @@
-package com.hackathon.hack.service;
-
-import com.hackathon.hack.Dto.LangchainResponseDto;
-
-public interface LangChainService {
-    public LangchainResponseDto langChainPost(String description);
-}
--- hack/hack/src/main/java/com/hackathon/hack/service/IGeminiService.java
+++
@@ -1,5 +0,0 @@
-package com.hackathon.hack.service;
-
-public interface IGeminiService {
-    public String analyzedImage(String base64Image);
-}
